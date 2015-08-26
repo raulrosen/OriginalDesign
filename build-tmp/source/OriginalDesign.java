f8aeff2acef4d050daf914039642ea2225fc7c12
unknown <rarosenfeld@697-334-Khamul.sf.k12.internal>
2015-08-26 11:08:30 -0700

    Changed to Dollar Bill

--- build-tmp/source/OriginalDesign.java
+++ build-tmp/source/OriginalDesign.java
@@ -1,8 +1,24 @@
-void setup()
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class OriginalDesign extends PApplet {
+
+public void setup()
 {
   size (400,400);
 }
-void draw()
+public void draw()
 {
   bill ();
   numbers();
@@ -11,13 +27,13 @@ void draw()
   writing();
 }
 
-void bill ()
+public void bill ()
 {
 	fill (30,200,30);
 	rect(50,50,300,150);
 }
 
-void numbers ()
+public void numbers ()
 {
 	fill (255);
 	textSize (22);
@@ -27,7 +43,7 @@ void numbers ()
 	text ("1",70,180);
 	text ("1",330,180);
 }
-void language ()
+public void language ()
 {
 	fill (255);
 	textSize (12);
@@ -37,7 +53,7 @@ void language ()
 	fill(0,0,0);
 	text ("THE UNITED STATES OF AMERICA", 90,80);
 }
-void president ()
+public void president ()
 {
 	fill (0,153,0);
 	ellipse (200,125,40,70);
@@ -48,7 +64,7 @@ void president ()
 	ellipse (205,125,2,2);
 	line (195,132,205,132);
 }
-void writing ()
+public void writing ()
 {
 	fill(0);
 	textSize (6);
@@ -59,4 +75,13 @@ void writing ()
 	fill (102,255,102);
 	text ("F19479269K",240,100);
 	text ("F19479269K",80,155);
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "OriginalDesign" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
